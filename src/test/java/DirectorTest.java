9fc6c949ebec289fc46a7ebc308b41bc814e830d
Angus Malloch <angusbass@hotmail.com>
2018-03-16 12:45:57 +0000

    Tested setters for Director

--- src/test/java/DirectorTest.java
+++ src/test/java/DirectorTest.java
@@ -10,11 +10,13 @@ public class DirectorTest {
 
 
     Film film;
+    Film film2;
     Director director;
 
     @Before
     public void before() {
         film = new Film("Terminator");
+        film2 = new Film("Frozen");
         director = new Director("Maven Gradle", 0, film);
     }
 
@@ -33,4 +35,23 @@ public class DirectorTest {
     public void canGetAssignedFilms() {
         assertEquals(film, director.getAssigned_films());
     }
+
+    @Test
+    public void canSetName() {
+        director.setName("Steven Spielberg");
+        assertEquals("Steven Spielberg", director.getName());
+    }
+
+    @Test
+    public void canSetCurrentPay() {
+        director.setCurrent_pay(100000);
+        assertEquals(100000, director.getCurrent_pay());
+    }
+
+    @Test
+    public void canSetFilm() {
+        director.setAssigned_films(film2);
+        assertEquals(film2, director.getAssigned_films());
+    }
+
 }
